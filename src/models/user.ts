9a7ea2ed66a593bbd377bde4d4093181cfff7a47
Achyuth Varma <achyuth.varma1@gmail.com>
2021-02-01 09:11:00 +0530

    Edited main user.ts file

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,38 +1,49 @@
 import mongoose from "mongoose";
 import { Password } from "../services/password";
 
-// An interface that describes a properties that required to create a new user
-interface UserAttr {
+// An interface that describes the properties
+// that are requried to create a new User
+interface UserAttrs {
   email: string;
   password: string;
 }
 
-// An interface to describe the properties that user model has
-
+// An interface that describes the properties
+// that a User Model has
 interface UserModel extends mongoose.Model<UserDoc> {
-  build(attr: UserAttr): UserDoc;
+  build(attrs: UserAttrs): UserDoc;
 }
 
-// An interfacce tat describe the properties that single User Document has
+// An interface that describes the properties
+// that a User Document has
 interface UserDoc extends mongoose.Document {
   email: string;
   password: string;
-  //   createdAt: string;
-  //   updatedAt: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
-// A middleware for mongodb when user saves the password. Now I'm hashing the password before saving it to the database
 userSchema.pre("save", async function (done) {
   if (this.isModified("password")) {
     const hashed = await Password.toHash(this.get("password"));
@@ -41,14 +52,10 @@ userSchema.pre("save", async function (done) {
   done();
 });
 
-userSchema.statics.build = (attr: UserAttr) => {
-  return new User(attr);
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
 };
 
 const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
-
-// const buildUser = (attr: UserAttr) => {
-//   return new User(attr);
-// };
 
 export { User };
