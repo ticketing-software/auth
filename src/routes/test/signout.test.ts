0ec921d4d072a50a1254d121ffd6ce3f64b80a0e
Achyuth Varma <achyuth.varma1@gmail.com>
2021-02-12 10:53:37 +0530

    Fixed Test Case

--- src/routes/test/signout.test.ts
+++ src/routes/test/signout.test.ts
@@ -24,6 +24,6 @@ it("To Test the signout and see whether cookie is deleted", async () => {
     .expect(200);
 
   expect(response.get("Set-Cookie")[0]).toEqual(
-    "express:sess=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly"
+    "express:sess=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT"
   );
 });
